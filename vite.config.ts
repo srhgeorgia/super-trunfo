937fca31061b5211b51b0161074106b64c7a17b1
sarah oliveira <sgeorgia0297@gmail.com>
2024-07-09 17:58:31 -0300

    publicDir vite

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import { defineConfig } from 'vite';
 export default defineConfig({
   root: '.', // Define o diretório raiz como o diretório principal
   base: './', // Define a base URL relativa
-  publicDir: 'public', // Diretório para arquivos estáticos
+  publicDir: 'assets', // Diretório para arquivos estáticos
   build: {
     outDir: 'dist', // Diretório de saída para a build
     rollupOptions: {
